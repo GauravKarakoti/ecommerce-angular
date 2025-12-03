bf47358b187029b464fa2bd8344c2e490a34b027
Gaurav Karakoti <karakotigaurav12@gmail.com>
2025-12-03 19:09:53 +0530

    SSR

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,22 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'product-details/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'checkout/payment/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: ':levelOne/:levelTwo/:levelThree',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'order/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
