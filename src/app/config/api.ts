e2c82b3eb0b41c243c1c4cb4025181ef48ed0a1d
Gaurav Karakoti <karakotigaurav12@gmail.com>
2025-12-03 19:14:21 +0530

    Environment Syncing

--- src/app/config/api.ts
+++ src/app/config/api.ts
@@ -1,3 +1,4 @@
-import { environment } from '../../environments/environment';
-
-export const BASE_API_URL = environment.apiUrl;
+const LOCAL = "http://localhost:5454";
+const DEPLOYED = "https://ecommerce-angular-5it4.onrender.com";
+// export const BASE_API_URL = LOCAL;
+export const BASE_API_URL = DEPLOYED;
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://ecommerce-angular-5it4.onrender.com'
-};
